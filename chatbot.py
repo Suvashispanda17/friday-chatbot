cd6e4d0a8775be5f854e5d7dbeb32cd2ac2d6416
Suvashispanda17 <pandasuvashis17@gmail.com>
2024-09-03 19:18:11 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,26 @@
+import google.generativeai as ai
+import pyttsx3 as px
+
+en=px.init()
+def speak(text):
+    en.say(text)
+    en.runAndWait()
+
+
+API_KEY='AIzaSyAk_TxZRPcsCoAGfrx2G_GYCqejSoUhHrY'
+ai.configure(api_key=API_KEY)
+model=ai.GenerativeModel("gemini-pro")
+chat= model.start_chat()
+
+
+while True:
+    message= input('you: ')
+    if message.lower() =="bye":
+        print("Friday: goodbye")
+        break
+    response=chat.send_message(message)
+    print("Friday: ",response.text)
+    speak(response.text)
+
+
+
